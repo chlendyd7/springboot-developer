5566e2c46e5d34e3eba67f6a26380fb891db7257
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-10-14 19:37:22 +0900

    security repo service init

---
+++ src/main/java/me/choiduyong/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,22 @@
+package me.choiduyong.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.choiduyong.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
+
