49443edeca0ec199a729591f30927a147bd3fe60
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-10-14 22:25:01 +0900

    user controller

---
+++ src/main/java/me/choiduyong/springbootdeveloper/controller/UserViewController.java
@@ -0,0 +1,15 @@
+package me.choiduyong.springbootdeveloper.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class UserViewController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+}
